a22ec610a8a4762a049f565df4d076a29fda79cf
Success Agent <miesineagent@gmail.com>
2023-01-30 14:17:37 +0100

    updated

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,26 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import Aos from "aos";
-import "aos/dist/aos.css";
 
 import { FaAngleUp } from "react-icons/fa";
 
 import { CgProfile } from "react-icons/cg";
 import { RxHamburgerMenu } from "react-icons/rx";
 
-const MobileNavbar = ({ pathname }) => {
+const MobileNavbar = () => {
   const [toggleNav, setToggleNav] = useState(false);
 
   const handleNav = () => {
     setToggleNav(!toggleNav);
   };
 
-  useEffect(() => {
-    Aos.init({
-      duration: 2000,
-    });
-    window.scrollTo(0, 0);
-  }, []);
   return (
     <div className="mobile-navbar">
       <div>
@@ -32,11 +24,7 @@ const MobileNavbar = ({ pathname }) => {
           <RxHamburgerMenu onClick={handleNav} />
         </div>
       </div>
-      <ul
-        className="mobile-navigation"
-        data-aos="fade-left"
-        data-visible={toggleNav}
-      >
+      <ul className="mobile-navigation" data-visible={toggleNav}>
         <div className="mobile-nav-flex">
           <Link to="/properties" className="link">
             Our Properties
