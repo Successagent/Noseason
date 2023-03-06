677c6f9aa66b488c5cedefe12b0ead374f31ded5
Success Agent <miesineagent@gmail.com>
2023-03-06 11:46:45 +0100

    updated

--- src/pages/Admin/Login.jsx
+++ src/pages/Admin/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
 
     if (logins.data !== "ERROR: 1") {
       localStorage.setItem("userDetails", logins.data);
-      navigate("/profile-settings");
+      navigate("/admin");
     }
   };
   return (
